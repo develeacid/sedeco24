8d8b76ab9045ee7a6d05460749e0c22c46a7fee2
gol.d.eleacid <ele.leader@gmail.com>
2024-03-08 16:38:05 -0600

    install AdminLTE3

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('adminlte::page')
+@section('title', 'Pandel de Aministración')
+@section('content_header')
+	<h1>Panel</h1>
+@stop
+@section('content')
+	<p>Bienvenido Administrador</p>
+@stop
+@section('css')
+	<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+@section('js')
+	<script> console.log('Hi!'); </script>
+@stop
