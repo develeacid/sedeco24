e64c342d388b7ec50ad086eaab2b68cc7463e20f
gol.d.eleacid <ele.leader@gmail.com>
2024-03-08 19:51:58 -0600

    advance settings jetstream and adminlte

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,31 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+ //CONFIGURACIONES ADMINLTE
+ public function adminlte_image(){
+    // recupera la imagen
+    $userImagePath = auth()->user()->profile_photo_path; 
+
+    // verifica si existe, ni no tiene devuelve una imagen aleatoria
+    if (!$userImagePath) {
+        return 'https:picsum.photos/300/300'; // url del ejemplo
+    }
+
+    // concatena con APP_URL y storage para mostrar la imagen
+    $fullImagePath = env('APP_URL') . '/' . 'storage' . '/' . $userImagePath;
+
+    // url completa
+    return $fullImagePath;
+}
+
+public function adminlte_desc(){
+    // recuperar cuando tengamos roles
+    return 'Administrador';
+}
+
+public function adminlte_profile_url(){
+    // perfil del usuario temporal de jetstream
+    return '/user/profile';
+}    
 }
